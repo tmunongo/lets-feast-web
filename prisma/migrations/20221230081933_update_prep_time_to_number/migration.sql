01bfb6f40b0a3e67c1b517a543911a71c33b23cf
Tawanda Munongo <ejmunongo@gmail.com>
2023-01-18 18:20:16 +0200

    delete old migrations

--- prisma/migrations/20221230081933_update_prep_time_to_number/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `prepTime` on the `Recipe` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Recipe" DROP COLUMN "prepTime",
-ADD COLUMN     "prepTime" INTEGER NOT NULL;
